158895ab7a4ab74c4285a016de401ad5c040d9f4
Christopher Schuette <christopher.schuette@remondis.de>
2019-05-23 16:19:58 +0200

    JavaDoc added.

--- src/main/java/com/remondis/cdc/consumer/pactbuilder/FieldBuilder.java
+++ src/main/java/com/remondis/cdc/consumer/pactbuilder/FieldBuilder.java
@@ -7,6 +7,16 @@ import au.com.dius.pact.consumer.dsl.PactDslJsonBody;
 
 public interface FieldBuilder<R, T> {
 
+  /**
+   * Specifies a custom {@link Function} that is used to translate the specified field to a structure in the
+   * {@link PactDslJsonBody}. The specified {@link Function} is responsible to define the right structures for the JSON
+   * representation. For example: If an object should be declared, the {@link Function} must invoke
+   * {@link PactDslJsonBody#object(String)} and {@link PactDslJsonBody#closeObject()}.
+   * 
+   * @param pactDslJsonBodyConfigurator The {@link Function} that takes a {@link PactDslJsonBody}, invokes declares the
+   *        correct structures on it and returns the resulting {@link DslPart}.
+   * @return Returns this builder for method chaining.
+   */
   ConsumerBuilder<T> as(Function<PactDslJsonBody, ? extends DslPart> pactDslJsonBodyConfigurator);
 
   ConsumerBuilder<T> as(String jsonFieldName);
--- src/test/java/com/remondis/cdc/consumer/pactbuilder/fieldtests/CustomFieldTest.java
+++ src/test/java/com/remondis/cdc/consumer/pactbuilder/fieldtests/CustomFieldTest.java
@@ -12,7 +12,27 @@ import au.com.dius.pact.consumer.dsl.PactDslJsonBody;
 public class CustomFieldTest {
 
   @Test
-  public void shouldCreateCustomStructure() throws Exception {
+  public void shouldCreateCustomStructure_embedded() throws Exception {
+    PactDslJsonBody pactDslJsonBody = new PactDslJsonBody();
+
+    Structure expected = Samples.Default.of(Structure.class)
+        .get();
+
+    pactDslJsonBody = ConsumerExpects.type(Structure.class)
+        .field(Structure::getString)
+        .as(pactBody -> {
+          return pactBody.stringType("string");
+        })
+        .build(pactDslJsonBody, expected);
+
+    Result<Parent> result = TestUtil.toObject(pactDslJsonBody, Parent.class);
+
+    TestUtil.assertObjectEqualsResult(expected, result);
+
+  }
+
+  @Test
+  public void shouldCreateCustomStructure_asReference() throws Exception {
     PactDslJsonBody pactDslJsonBody = new PactDslJsonBody();
 
     Parent parent = Samples.Default.of(Parent.class)
