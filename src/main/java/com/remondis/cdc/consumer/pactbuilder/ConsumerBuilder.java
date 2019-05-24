51fb7c93e0ed21b0d3293eaa9479710463bb8fba
Christopher Schuette <christopher.schuette@remondis.de>
2019-05-24 13:48:25 +0200

    JavaDoc for ConsumerBuilder

--- src/main/java/com/remondis/cdc/consumer/pactbuilder/ConsumerBuilder.java
+++ src/main/java/com/remondis/cdc/consumer/pactbuilder/ConsumerBuilder.java
@@ -4,14 +4,55 @@ import au.com.dius.pact.consumer.dsl.PactDslJsonBody;
 
 public interface ConsumerBuilder<T> {
 
+  /**
+   * Registers a global type mapping. Each occurrence of the specified type will be converted to a JSON structure by
+   * applying the specified modifier.
+   * <p>
+   * <b>Note:</b> The specified {@link PactDslModifier} is responsible to define the right
+   * structures for the JSON
+   * representation. For example: If an object should be declared, the {@link PactDslModifier} must invoke
+   * {@link PactDslJsonBody#object(String)} and {@link PactDslJsonBody#closeObject()}.
+   * </p>
+   * 
+   * @param <S> The type to convert.
+   * @param type The type to convert.
+   * @param modifier The {@link PactDslModifier} that creates the JSON structures.
+   * @return Returns this instance for method chaining.
+   */
   <S> ConsumerBuilder<T> useTypeMapping(Class<S> type, PactDslModifier<S> modifier);
 
+  /**
+   * Selects a field to specify a JSON mapping. Global type mappings added by
+   * {@link #useTypeMapping(Class, PactDslModifier)}, {@link #referencing(ConsumerBuilder)} or default type mappings
+   * will be overridden by field configurations.
+   * 
+   * @param <R> The field type.
+   * @param fieldSelector A lambda that performs a get-call on the target object, to select a field.
+   * @return Returns this instance for method chaining.
+   */
   <R> FieldBuilder<R, T> field(TypedSelector<R, T> fieldSelector);
 
+  /**
+   * Registers another {@link ConsumerBuilder} that specifies how a type should be mapped to a JSON structure.
+   * 
+   * @param consumer The {@link ConsumerBuilder} instance specifying the JSON structure of a type.
+   * @return Returns this instance for method chaining.
+   */
   ConsumerBuilder<T> referencing(ConsumerBuilder<?> consumer);
 
+  /**
+   * Takes the specified {@link PactDslJsonBody} instance and creates the JSON structure.
+   * 
+   * @param pactDslJsonBody The {@link PactDslJsonBody} to build.
+   * @param sampleData The sample data instance that provides values for each field of the type.
+   * @return Returns a {@link PactDslJsonBody} representing the JSON structure of the type, this {@link ConsumerBuilder}
+   *         was configured for.
+   */
   PactDslJsonBody build(PactDslJsonBody pactDslJsonBody, T sampleData);
 
+  /**
+   * @return Returns the type this {@link ConsumerBuilder} is using.
+   */
   Class<T> getType();
 
 }
