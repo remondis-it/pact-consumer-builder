3d3765457e35aa3e68146ff053d9550bf48cd916
Christopher Schuette <christopher.schuette@remondis.de>
2020-02-17 14:54:18 +0100

    Bugfix: Analyze generics at runtime
    - Fix test case
    - Recursive call getCollectionModifier
    - Refactor getCollectionModifier
    - Further test cases.

---
+++ src/test/java/com/remondis/cdc/consumer/pactbuilder/regression/generics/Dummy.java
@@ -0,0 +1,28 @@
+package com.remondis.cdc.consumer.pactbuilder.regression.generics;
+
+/**
+ * @author schuettec
+ *
+ */
+public class Dummy {
+
+  private String string;
+
+  public Dummy() {
+    super();
+  }
+
+  public Dummy(String string) {
+    super();
+    this.string = string;
+  }
+
+  public String getString() {
+    return string;
+  }
+
+  public void setString(String string) {
+    this.string = string;
+  }
+
+}
